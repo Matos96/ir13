08c20385b078715f66420075f34e08e76e8fcdce
Christian Wemstad <wemstad@kth.se>
2013-02-27 10:34:45 +0100

    Removed spacings and added titlesdocument

--- ir/Query.java
+++ ir/Query.java
@@ -1,9 +1,9 @@
-/*  
+/*
  *   This file is part of the computer assignment for the
  *   Information Retrieval course at KTH.
- * 
+ *
  *   First version:  Hedvig Kjellström, 2012
- */  
+ */
 
 package ir;
 
@@ -11,16 +11,16 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Query {
-    
+
     public LinkedList<String> terms = new LinkedList<String>();
     public LinkedList<Double> weights = new LinkedList<Double>();
 
     /**
-     *  Creates a new empty Query 
+     *  Creates a new empty Query
      */
     public Query() {
 	}
-	
+
     /**
      *  Creates a new Query from a string of words
      */
@@ -29,16 +29,16 @@ public class Query {
 		while ( tok.hasMoreTokens() ) {
 			terms.add( tok.nextToken() );
 			weights.add( new Double(1) );
-		}    
+		}
 	}
-	
+
     /**
      *  Returns the number of terms
      */
     public int size() {
 		return terms.size();
 	}
-	
+
     /**
      *  Returns a shallow copy of the Query
      */
@@ -48,18 +48,16 @@ public class Query {
 		queryCopy.weights = (LinkedList<Double>) weights.clone();
 		return queryCopy;
 	}
-	
+
     /**
      *  Expands the Query using Relevance Feedback
      */
     public void relevanceFeedback( PostingsList results, boolean[] docIsRelevant, Indexer indexer ) {
 		// results contain the ranked list from the current search
 		// docIsRelevant contains the users feedback on which of the 10 first hits are relevant
-		
+
 		//
 		//  YOUR CODE HERE
 		//
     }
 }
-
-    
